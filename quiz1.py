871e971b444b17bcb22a957c61c20fa8af0c4895
stepbystep <likerberi97@gmail.com>
2019-07-15 16:24:02 +0900

    pre-processing-ing

--- quiz1.py
+++ quiz1.py
@@ -23,9 +23,62 @@ def bar_chart(feature) :
     plt.boxplot(df.values)
     plt.show()
 
-bar_chart('Pclass')
-bar_chart('Sex')
-# bar_chart('Age')
-# bar_chart('SibSp')
-# bar_chart('Parch')
-bar_chart('Embarked')
+# bar_chart('Pclass')
+# bar_chart('Sex')
+# # bar_chart('Age')
+# # bar_chart('SibSp')
+# # bar_chart('Parch')
+# bar_chart('Embarked')
+
+
+# pre-processing
+train = train.drop(['Cabin'], axis=1)
+train = train.drop(['Ticket'], axis=1)
+
+test = test.drop(['Cabin'], axis=1)
+test = test.drop(['Ticket'], axis=1)
+
+# print(train.head())
+
+train_test_merge = [train, test]
+
+# Name area, categorizing.
+for dataset in train_test_merge:
+    dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.')
+
+# print(train.head(5))
+# print(pd.crosstab(train['Title'], train['Sex']))
+
+for dataset in train_test_merge:
+    dataset['Title'] = dataset['Title'].replace([
+        'Capt', 'Col', 'Countess', 'Don', 'Jonkheer', 'Lady', 'Major', 'Rev', 'Sir', 'Dr'
+    ], 'Other')
+    # French -> English Mlle, Mme
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    # No Discrimination but pre-category...
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+
+print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+for dataset in train_test_merge:
+    dataset['Title'] = dataset['Title'].astype(str)
+
+    # Sex
+    dataset['Sex'] = dataset['Sex'].astype(str)
+
+    # Embarked (departing from)
+    dataset['Embarked'] = dataset['Embarked'].fillna('S') # there are only 2 loss data.
+
+train['Age'] = train['Age'].fillna(-0.5)
+test['Age'] = test['Age'].fillna(-0.5)
+bins = [-1, 0, 18, 66, 80, 100, 123] # , criteria from UN
+labels = ['Unknown', 'Minor', 'Youth', 'Middle', 'Old', 'Longlived']
+train['AgeGroup'] = pd.cut(train['Age'], bins, labels = labels)
+test['AgeGroup'] = pd.cut(train['Age'], bins, labels = labels)
+
+print(train.head(5))
+
+
+
+
